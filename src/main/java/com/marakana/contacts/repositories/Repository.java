b205fe1f8a8ed8606063890ca99fe6f46dda4d44
nawfalmoro <frejnawfal@hotmail.com>
2013-12-21 13:46:03 +0000

    Adding hibernate JPA

---
+++ src/main/java/com/marakana/contacts/repositories/Repository.java
@@ -0,0 +1,29 @@
+package com.marakana.contacts.repositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class Repository<E> {
+	
+	
+	private final EntityManager em = Persistence.createEntityManagerFactory(
+			"training").createEntityManager();
+	private final Class<E> entityClass;
+	
+	public Repository(Class<E> entityClass){
+		this.entityClass=entityClass;
+	}
+	public void save(E entity) {
+		em.merge(entity);
+	}
+
+
+	public void delete(E entity) {
+		em.remove(entity);
+
+	}
+
+	public E find(long id) {
+		return em.find(entityClass, id);
+	}
+}
--- src/main/java/com/marakana/contacts/servlets/ContactServlet.java
+++ src/main/java/com/marakana/contacts/servlets/ContactServlet.java
@@ -1,7 +1,6 @@
 package com.marakana.contacts.servlets;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -32,23 +31,16 @@ public class ContactServlet extends HttpServlet {
 					response);
 		} else {
 			Long id = Long.parseLong(request.getParameter("id"));
-			try {
-				Contact contact = contactRepository.find(id);
-				Address address = addressRepository
-						.find(contact.getAdresseId());
-				request.setAttribute("contact", contact);
-				request.setAttribute("address", address);
-				if (request.getParameter("edit") != null) {
-					request.getRequestDispatcher("/jsp/editContact.jsp")
-							.forward(request, response);
-				} else
-					request.getRequestDispatcher("/jsp/viewContact.jsp")
-							.forward(request, response);
-
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Contact contact = contactRepository.find(id);
+			Address address = addressRepository.find(contact.getAdresseId());
+			request.setAttribute("contact", contact);
+			request.setAttribute("address", address);
+			if (request.getParameter("edit") != null) {
+				request.getRequestDispatcher("/jsp/editContact.jsp").forward(
+						request, response);
+			} else
+				request.getRequestDispatcher("/jsp/viewContact.jsp").forward(
+						request, response);
 		}
 
 	}
@@ -62,42 +54,29 @@ public class ContactServlet extends HttpServlet {
 					request.getParameter("state"),
 					request.getParameter("street"), Integer.parseInt(request
 							.getParameter("zip")));
-			try {
-				addressRepository.create(address);
-				Contact contact = new Contact(request.getParameter("name"),
-						address.getId());
-				contactRepository.create(contact);
-				response.sendRedirect("/contactList");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if(request.getParameter("delete") != null){
-			try {
-				Contact contact = contactRepository.find(Long.parseLong(request.getParameter("id")));
-				Address address = addressRepository.find(contact.getAdresseId());
-				contactRepository.delete(contact);
-				addressRepository.delete(address);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			addressRepository.save(address);
+			Contact contact = new Contact(request.getParameter("name"),
+					address.getId());
+			contactRepository.save(contact);
 			response.sendRedirect("/contactList");
-		}else if (request.getParameter("save") != null) {
+		} else if (request.getParameter("delete") != null) {
+			Contact contact = contactRepository.find(Long.parseLong(request
+					.getParameter("id")));
+			Address address = addressRepository.find(contact.getAdresseId());
+			contactRepository.delete(contact);
+			addressRepository.delete(address);
+			response.sendRedirect("/contactList");
+		} else if (request.getParameter("save") != null) {
 			Address address = new Address(request.getParameter("city"),
 					request.getParameter("state"),
 					request.getParameter("street"), Integer.parseInt(request
 							.getParameter("zip")));
 			address.setId(Long.parseLong(request.getParameter("adresseId")));
-			Contact contact = new Contact(request.getParameter("name"), Long.parseLong(request.getParameter("adresseId")));
+			Contact contact = new Contact(request.getParameter("name"),
+					Long.parseLong(request.getParameter("adresseId")));
 			contact.setId(Long.parseLong(request.getParameter("id")));
-			try {
-				addressRepository.update(address);
-				contactRepository.update(contact);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			addressRepository.save(address);
+			contactRepository.save(contact);
 			response.sendRedirect("/contact?id=" + contact.getId());
 		}
 	}
