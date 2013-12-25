3809e92d0b216d5430cecbef28a128d7bc8a0704
nawfalmoro <frejnawfal@hotmail.com>
2013-12-25 19:09:54 +0000

    Fixing delete company by cascading deleting to offices

--- src/main/java/com/marakana/contacts/entities/Company.java
+++ src/main/java/com/marakana/contacts/entities/Company.java
@@ -5,10 +5,12 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import javax.persistence.CascadeType;
+
 @Entity
 public class Company extends Contact {
 
-	@OneToMany(mappedBy = "company")
+	@OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
 	Set<Office> offices;
 
 	public Company() {
